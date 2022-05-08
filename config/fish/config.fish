b56c530faa634cb463697c0836f15183d53dc08b
Maskrpone <deparis.hipp@gmail.com>
2022-05-08 21:48:22 +0200

    backup

---
+++ config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+alias l "exa -l --icons"
+alias q "exit"
+alias n "nvim"
+alias g "git"
+alias \:q "exit"
+alias f "fzf"
+alias r "ranger"
---
+++ config/polybar/launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+# polybar desktop &
+# 
+# polybar sets &
+# 
+# polybar info &
+# 
+# polybar date &
+# 
+# polybar spotify &
+
+polybar taskbar &
+
+echo "Bars launched..."
